f9e2fb1b4fbef4ed27928753a14eb798400f4418
khauta seabata <75206578+KhautaSeabata@users.noreply.github.com>
2025-05-15 09:05:12 -0800

    Update firebase_client.py

--- app/firebase_client.py
+++ app/firebase_client.py
@@ -1,12 +1,6 @@
-import os
-import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
-cred_dict = json.loads(cred_json)
-
-cred = credentials.Certificate(cred_dict)
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
